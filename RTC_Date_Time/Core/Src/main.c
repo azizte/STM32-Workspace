801940c0635beb5fb7f74da51b9030c126519b7b
azizte <aziztelim@gmail.com>
2024-11-06 18:19:56 +0300

    LED toggle

--- RTC_Date_Time/Core/Src/main.c
+++ RTC_Date_Time/Core/Src/main.c
@@ -99,7 +99,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
